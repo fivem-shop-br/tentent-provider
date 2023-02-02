dc044d110f44206d26346a99e65f92c214e3dfd3
Fulano de Tal <F3ZINH0HG@gmail.com>
2023-02-02 13:11:11 -0300

    refactor: production test.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,79 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    pageExtensions: ["page.tsx"],
+    reactStrictMode: true,
+    pageExtensions: ["page.tsx", "api.ts", "api.tsx"],
     images: {
         domains: ["five-m.store"],
+    },
+    async rewrites() {
+        return {
+            afterFiles: [{
+                    has: [{
+                        type: "host",
+                        value: "(?<host>.*)",
+                    }, ],
+                    source: "/",
+                    destination: "/hosts/:host",
+                },
+                {
+                    has: [{
+                            type: "host",
+                            value: "(?<host>.*)",
+                        },
+                        {
+                            type: "header",
+                            key: "accept-encoding",
+                            value: "(?<encoding>.*)",
+                        },
+                        {
+                            type: "query",
+                            key: "x",
+                            value: "(?<x>.*)",
+                        },
+                    ],
+                    source: "/headers/:slug*",
+                    destination: "/headers/:slug*/host/:host/encoding/:encoding/x/:x",
+                },
+                {
+                    has: [{
+                            type: "host",
+                            value: "(?<host>.*)",
+                        },
+                        {
+                            type: "header",
+                            key: "accept-encoding",
+                            value: "(?<encoding>.*)",
+                        },
+                    ],
+                    source: "/headers/:slug*",
+                    destination: "/headers/:slug*/host/:host/encoding/:encoding/x/null",
+                },
+                {
+                    has: [{
+                        type: "host",
+                        value: "(?<host>.*)",
+                    }, ],
+                    source: "/headers/:slug*",
+                    destination: "/headers/:slug*/host/:host/encoding/null/x/null",
+                },
+                {
+                    has: [{
+                        type: "host",
+                        value: "(?<host>.*)",
+                    }, ],
+                    source: "/posts/:id",
+                    destination: "/hosts/:host/posts/:id",
+                },
+                {
+                    has: [{
+                        type: "host",
+                        value: "(?<host>.*)",
+                    }, ],
+                    source: "/slugs/:slug*",
+                    destination: "/hosts/:host/slugs/:slug*",
+                },
+            ],
+        };
     },
 };
 
