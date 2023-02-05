c369ed360a7f193fec4f5ed2228c14543374569f
Fulano de Tal <F3ZINH0HG@gmail.com>
2023-02-05 20:39:17 -0300

    feat: add react-query-provider.

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,7 +1,13 @@
+import { queryClient } from "@src/services/query-client";
 import type { AppProps } from "next/app";
 import { globalStyles } from "../styles/global.css";
+import { QueryClientProvider } from "react-query";
 
 globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
