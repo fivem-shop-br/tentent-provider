03bc8e60053c65e038fe6866d834d414416fb066
Fulano de Tal <F3ZINH0HG@gmail.com>
2023-02-05 20:35:58 -0300

    feat: add env in url api.

--- next.config.js
+++ next.config.js
@@ -1,80 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    pageExtensions: ["page.tsx", "api.ts", "api.tsx"],
-    images: {
-        domains: ["five-m.store"],
-    },
-    async rewrites() {
-        return {
-            afterFiles: [{
-                    has: [{
-                        type: "host",
-                        value: "(?<host>.*)",
-                    }, ],
-                    source: "/",
-                    destination: "/hosts/:host",
-                },
-                {
-                    has: [{
-                            type: "host",
-                            value: "(?<host>.*)",
-                        },
-                        {
-                            type: "header",
-                            key: "accept-encoding",
-                            value: "(?<encoding>.*)",
-                        },
-                        {
-                            type: "query",
-                            key: "x",
-                            value: "(?<x>.*)",
-                        },
-                    ],
-                    source: "/headers/:slug*",
-                    destination: "/headers/:slug*/host/:host/encoding/:encoding/x/:x",
-                },
-                {
-                    has: [{
-                            type: "host",
-                            value: "(?<host>.*)",
-                        },
-                        {
-                            type: "header",
-                            key: "accept-encoding",
-                            value: "(?<encoding>.*)",
-                        },
-                    ],
-                    source: "/headers/:slug*",
-                    destination: "/headers/:slug*/host/:host/encoding/:encoding/x/null",
-                },
-                {
-                    has: [{
-                        type: "host",
-                        value: "(?<host>.*)",
-                    }, ],
-                    source: "/headers/:slug*",
-                    destination: "/headers/:slug*/host/:host/encoding/null/x/null",
-                },
-                {
-                    has: [{
-                        type: "host",
-                        value: "(?<host>.*)",
-                    }, ],
-                    source: "/posts/:id",
-                    destination: "/hosts/:host/posts/:id",
-                },
-                {
-                    has: [{
-                        type: "host",
-                        value: "(?<host>.*)",
-                    }, ],
-                    source: "/slugs/:slug*",
-                    destination: "/hosts/:host/slugs/:slug*",
-                },
-            ],
-        };
-    },
+  reactStrictMode: false,
+  pageExtensions: ["page.tsx"],
+  images: {
+    domains: ["static.fivem.website"],
+  },
+  env: {
+    API_URL: process.env.API_URL,
+  },
 };
 
 module.exports = nextConfig;
